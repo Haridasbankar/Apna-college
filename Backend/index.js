48c5f33d4c79e4b2550b1e102bd250c2c88e2025
Haridas Bankar <bankarhari02@gmail.com>
2025-08-12 09:31:37 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,15 @@
-// let obj = {
-//     name: "haridas",
-//     age: 12,
-//     class: "12th"0
-// }
+let obj = {
+    name: "haridas",
+    age: 12,
+    class: "12th"
+}
 
-// console.log(obj);
-// console.log(process.argv);
-// let argv = process.argv;
-// for (let i = 2; i < argv.length; i++) {
-//     console.log("hellot to ", argv[i]);
-// }
+console.log(obj);
+console.log(process.argv);
+let argv = process.argv;
+for (let i = 2; i < argv.length; i++) {
+    console.log("hellot to ", argv[i]);
+}
 
 const somevalue = require("./math");
 console.log(somevalue.sum(2, 2));
